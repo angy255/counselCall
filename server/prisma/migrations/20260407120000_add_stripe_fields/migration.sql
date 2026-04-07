1ff845e82ea44621b8ae7a0900148359687a2a18
Angelica Matos <angelicamatos255@gmail.com>
2026-04-06 22:18:48 -0400

    rebaseline prisma migrations and preserve app-table RLS

--- server/prisma/migrations/20260407120000_add_stripe_fields/migration.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE "AttorneyProfile"
-ADD COLUMN "stripeAccountId" TEXT,
-ADD COLUMN "stripeOnboardingComplete" BOOLEAN NOT NULL DEFAULT false;
-
-ALTER TABLE "Booking"
-ADD COLUMN "paymentIntentId" TEXT,
-ADD COLUMN "amountInCents" INTEGER;
